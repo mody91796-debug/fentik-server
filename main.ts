b846065f5bb6b54580fabe1fe94761831135fa6f
mody91796-debug <mody91796@gmail.com>
2026-06-01 21:34:18 +0300

    Switch from serve to Deno.serve for HTTP handling

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
-import { serve } from "https://deno.land/std@0.224.0/http/server.ts";
-
-serve(async (req) => {
+Deno.serve(async (req) => {
   const url = new URL(req.url);
   
   const corsHeaders = {
